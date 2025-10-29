90ea38f25d66ae144696835c864e4d9330bb1588
Olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2025-10-29 14:44:57 -0700

    chore: add Terraform variables file placeholder

---
+++ variables.tf
@@ -0,0 +1,9 @@
+
+variable "subscription_id" {
+  type    = string
+  default = "501c458a-5def-42cf-bbb8-c75078c1cdbc"
+}
+variable "tenant_id" {
+  type    = string
+  default = "776f9ea5-7add-469d-bc51-8e855e9a1d26"
+}
